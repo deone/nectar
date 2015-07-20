b94c977a4c7bd82ca7a43eb88314341d82980484
Oladayo Osikoya <alwaysdeone@gmail.com>
2015-07-20 15:43:06 +0100

    Add models for Products. Add products to INSTALLED_APPS setting. Add MEDIA_ROOT setting.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+# product lines = categories
+# brands
+# products
+
+class Common(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        abstract = True
+
+class Category(Common):
+    pass
+
+class SubCategory(Common):
+    category = models.ForeignKey(Category)
+
+class Brand(Common):
+    category = models.ForeignKey(Category)
+    image = models.ImageField(upload_to="brand_images")
+
+class Product(Common):
+    category = models.ForeignKey(Category)
+    brand = models.ForeignKey(Brand)
+    code = models.CharField(max_length=50, unique=True)
+    image = models.ImageField(upload_to="product_images")
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
