902aa0033181c1cf36beac72e3b7f238d9226f02
Oladayo Osikoya <alwaysdeone@gmail.com>
2015-07-24 16:19:28 +0100

    Remove commented lines. This was omitted from the previous commit.

--- products/views.py
+++ products/views.py
@@ -4,17 +4,14 @@ from .models import *
 
 class ProductList(ListView):
     queryset = Product.objects.all()
-    # template_name = 'products/index.html'
     context_object_name = 'new_products'
 
 class ProductDetail(DetailView):
     model = Product
-    # template_name = 'products/detail.html'
     context_object_name = 'product'
 
     def get_context_data(self, **kwargs):
         context = super(ProductDetail, self).get_context_data(**kwargs)
-        # related products
         context['related_products'] = Product.objects.all()[:4]
         return context
 
