6b4ebead0c489147e2cb6bb2e8389cc711fed735
Oladayo Osikoya <dayo@incisia.com>
2015-08-26 00:09:09 +0100

    wsgi and settings for production.

--- nectar/settings.py
+++ nectar/settings.py
@@ -105,12 +105,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = "/home/deone/webapps/nectar_static"
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = '/Users/deone/uploads/nectar/'
 
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
