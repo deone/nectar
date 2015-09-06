ba1dbf839d450fd437225a12bfb95a5b7979223d
Dayo Osikoya <alwaysdeone@gmail.com>
2015-09-06 22:31:40 +0100

    Reduce size of social media links. Display feedback on contact page if request.method is POST. Wrap categories in .row.

--- contact/views.py
+++ contact/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import render
 from .forms import ContactForm
 
 def index(request):
+    context = {}
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            pass
+            context.update({'feedback': 'Thank you for contacting Nectar Beauty Hub. We will respond to your enquiry shortly.'})
     else:
         form = ContactForm()
 
-    context = {'form': form}
+    context.update({'form': form})
     return render(request, 'contact/index.html', context)
